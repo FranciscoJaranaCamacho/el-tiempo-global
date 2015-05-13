eac4a96d67a3f26182f3f8fbe07391c5eaa631f2
Francisco Jarana Camacho <franciscojarana92@gmail.com>
2015-05-13 23:30:15 +0100

    modificar bottle

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -11,7 +11,7 @@ def consulta():
 	tipo=request.form.post("tipo")
 	URL_BASE='http://api.worldweatheronline.com/free/v2/ski.ashx'
 	dic={'key':'62dfc963c650d5c89965ac2480534','q':localizacion,'date':fecha,'format':'json'}
-	inf= requests.post (URL_BASE,params=dic)
+	inf=request.post(URL_BASE,params=dic)
 	return template("template_con.tpl",localizacion=localizacion,fecha=fecha,tipo=tipo,inf=inf['data']['weather'][0])
 	
 # This must be added in order to do correct path lookups for the views
