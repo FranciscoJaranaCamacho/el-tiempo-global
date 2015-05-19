5cac172e22ca2478240413bd7458b2ff99c87359
Francisco Jarana Camacho <franciscojarana92@gmail.com>
2015-05-19 09:59:23 +0100

    Editar bottle

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -12,11 +12,10 @@ def consulta():
 	tipo=request.forms.get("tipo")
 	URL_BASE='http://api.worldweatheronline.com/free/v2/ski.ashx'
 	dic={'key':'62dfc963c650d5c89965ac2480534','q':localizacion,'date':fecha,'format':'json'}
-	infor=requests.post(URL_BASE,params=dic)
+	infor=requests.get(URL_BASE,params=dic)
 	datos=infor.json
-	general=datos
-	return template("template_con.tpl",localizacion=localizacion,fecha=fecha,tipo=tipo,inf=datos['data']['weather'][0])
-	
+	#return template("template_con.tpl",localizacion=localizacion,fecha=fecha,tipo=tipo,inf=datos['data']['weather'][0])
+	return template("template_con.tpl",localizacion=localizacion,fecha=fecha,tipo=tipo)
 # This must be added in order to do correct path lookups for the views
 import os
 from bottle import TEMPLATE_PATH
