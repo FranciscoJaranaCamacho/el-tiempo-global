208429cebe573a228e3295b35ed94c70fcb63c92
Francisco Jarana Camacho <franciscojarana92@gmail.com>
2015-06-02 11:15:42 +0100

    Editar bottle

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -27,7 +27,7 @@ def consulta1(localizacion,tipo):
 	datos=infor.json()
 	return template("template_con.tpl",localizacion=localizacion,fecha=fecha,tipo=tipo,datos=datos)
 	
-@post('/vertiempo_ayer/<localizacion>/<fecha>/<tipo>')
+@route('/vertiempo_ayer/<localizacion>/<fecha>/<tipo>')
 def consulta2(localizacion, fecha, tipo):
 	fecha=fecha-timedelta(days=1)
 	URL_BASE='http://api.worldweatheronline.com/free/v2/ski.ashx'
@@ -36,7 +36,7 @@ def consulta2(localizacion, fecha, tipo):
 	datos=infor.json()
 	return template("template_con.tpl",localizacion=localizacion,fecha=fecha,tipo=tipo,datos=datos)
 
-@post('/vertiempo_mes_pasado/<localizacion>/<fecha>/<tipo>')
+@route('/vertiempo_mes_pasado/<localizacion>/<fecha>/<tipo>')
 def consulta3(localizacion, fecha, tipo):
 	fecha=fecha-timedelta(days=30)
 	URL_BASE='http://api.worldweatheronline.com/free/v2/ski.ashx'
@@ -45,7 +45,7 @@ def consulta3(localizacion, fecha, tipo):
 	datos=infor.json()
 	return template("template_con.tpl",localizacion=localizacion,fecha=fecha,tipo=tipo,datos=datos)
 
-@post('/vertiempo_anio_pasado/<localizacion>/<fecha>/<tipo>')
+@route('/vertiempo_anio_pasado/<localizacion>/<fecha>/<tipo>')
 def consulta4(localizacion, fecha, tipo):
 	fecha=fecha-timedelta(days=365)
 	URL_BASE='http://api.worldweatheronline.com/free/v2/ski.ashx'
@@ -54,7 +54,7 @@ def consulta4(localizacion, fecha, tipo):
 	datos=infor.json()
 	return template("template_con.tpl",localizacion=localizacion,fecha=fecha,tipo=tipo,datos=datos)
 
-@post('/vertiempo_despues/<localizacion>/<fecha>/<tipo>')
+@route('/vertiempo_despues/<localizacion>/<fecha>/<tipo>')
 def consulta5(localizacion, fecha, tipo):
 	fecha=fecha+timedelta(days=1)
 	URL_BASE='http://api.worldweatheronline.com/free/v2/ski.ashx'
